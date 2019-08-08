fda10f28c0ddecba9ec3b749087e49943afbe8fe
Florian Schäfer <florian.schaefer@securepoint.de>
2019-08-08 09:40:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# node-red-contrib-authed-websocket
+A websocket input and output node that rejects connections based on a external authentication flow.
+
+Install
+-------
+
+Run the following command in the root directory of your Node-RED install
+
+    npm install node-red-contrib-authed-websocket
+
+
+Usage
+-----
+
+To be written...
---
+++ index.ts
@@ -0,0 +1,157 @@
+const http = require("http")
+const WebSocket = require("ws")
+const url = require('url')
+const uuidv4 = require('uuid/v4')
+
+
+function normalizePath(path: string) {
+    return "/" + String(path).split(/[/\\]+/).filter(String).join("/")
+}
+
+let clientHashMap: any = {}
+
+module.exports = function (RED: any) {
+
+    function authenticate(config: any, request: any, fn: (err: boolean, client: any) => void) {
+        const pathname = url.parse(request.url).pathname
+        let authed = normalizePath(pathname) === normalizePath(config.pathname)
+        let error = !authed
+        fn(error, {})
+    }
+
+	function AuthedWebsocketInputNode(this: any, config: any) {
+
+        RED.nodes.createNode(this, config)
+
+        const webserver = RED.nodes.getNode(config.webserver)
+        const wss = new WebSocket.Server({ noServer: true })
+
+        wss.on("connection", (ws: any, request: any, clientId: string) => {
+
+            clientHashMap[clientId].connection.socket = ws
+
+            ws.on("close", () => {
+                delete clientHashMap[clientId]
+            })
+
+            ws.on("message", (message: any) => {
+                console.log(`Received message ${message} from user ${clientId}`)
+                let msg = {
+                    "payload": message,
+                    "_clientid": clientId,
+                }
+                try {
+                    msg.payload = JSON.parse(message)
+                }
+                catch(e) {}
+                this.send([undefined, msg])
+            })
+        })
+
+        webserver.server.on('upgrade', (request: any, socket: any, head: any) => {
+            let clientId = uuidv4()
+            let msg = {
+                "payload": "connect",
+                "_clientid": clientId,
+                "_headers": request.headers
+            }
+
+            clientHashMap[clientId] = {
+                "upgrade":  {
+                    "request": request,
+                    "socket": socket,
+                    "head": head
+                },
+                "connection": {
+                    "socket": undefined
+                }
+            }
+
+            this.send([msg, undefined])
+        })
+            
+        this.on('input', (message: any) => {
+            let clientId = message._clientid
+            if (clientId && clientHashMap[clientId] && clientHashMap[clientId].upgrade) {
+                let request = clientHashMap[clientId].upgrade.request
+                let socket = clientHashMap[clientId].upgrade.socket
+                let head = clientHashMap[clientId].upgrade.head
+                if (message.payload == "reject") {
+                    socket.destroy()
+                    delete clientHashMap[clientId]
+                }
+                else {
+                    wss.handleUpgrade(request, socket, head, (ws: any) => {
+                        wss.emit('connection', ws, request, clientId)
+                    })
+                }
+            }
+        })
+    }
+
+    RED.nodes.registerType("authed-websocket-input-node", AuthedWebsocketInputNode)
+
+
+	function AuthedWebsocketOutputNode(this: any, config: any) {
+
+        RED.nodes.createNode(this, config)
+
+        this.on('input', (message: any) => {
+            let ws
+            if (!message) {
+                /* no message */
+            }
+            else if (!clientHashMap[message._clientid]) {
+                /* client not in hashmap? */
+            }
+            else if (!clientHashMap[message._clientid].connection) {
+                /* no websocket connection object in hasmap? */
+            }
+            else if ((ws = clientHashMap[message._clientid].connection.socket) == null) {
+                /* no websocket */
+            }
+            else if (ws.readyState != WebSocket.OPEN) {
+                /* not open */
+            }
+            else {
+                let msg = typeof message.payload == "object"
+                    ? JSON.stringify(message.payload)
+                    : message.payload
+
+                if (message.terminate) {
+                    ws.terminate()
+                }
+                else {
+                    ws.send(msg)
+                }
+            }
+        })
+
+    }
+
+    RED.nodes.registerType("authed-websocket-output-node", AuthedWebsocketOutputNode)
+
+
+    function RemoteWebServerNode(this: any, config: any) {
+        RED.nodes.createNode(this, config)
+        this.port = config.port
+
+        this.server = http.createServer()
+        this.server.listen(this.port)
+
+        this.on("close", () => {
+            Object.keys(clientHashMap).forEach((clientId: string) => {
+                if (clientHashMap[clientId] && clientHashMap[clientId].connection && clientHashMap[clientId].connection.socket) {
+                    clientHashMap[clientId].connection.socket.terminate()
+                }
+                else if (clientHashMap[clientId] && clientHashMap[clientId].upgrade && clientHashMap[clientId].upgrade.socket) {
+                    clientHashMap[clientId].upgrade.socket.destroy()
+                }
+            })
+            this.server.close()
+        })
+    }
+
+    RED.nodes.registerType("authed-websocket-remote-webserver", RemoteWebServerNode)
+}
+
